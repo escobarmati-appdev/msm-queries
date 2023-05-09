6c7f6f83283493abfa0f076a5ad5e16d149d5ec3
Matias Escobar <mescobar@chicagobooth.edu>
2023-05-09 16:56:59 +0000

    Done

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -11,10 +11,21 @@ class ActorsController < ApplicationController
     # params {"an_id"=>"youngest"} 
 
     @list_of_actors = Actor.all
+    @list_of_characters = Character.all
+    @list_of_movies = Movie.all
 
     @the_id = params.fetch("actor_id")
     @the_actor = Actor.where({ :id => @the_id }).at(0) #get the actor ID
-    @character = Character.where({ :actor_id => @the_actor.id }).at(0) #get the character
+    @characters = Character.where({ :actor_id => @the_actor.id }) #get the character
+
+
+
+
+    # the_id = params.fetch("an_id")
+    # @the_director = Director.where({ :id => the_id }).at(0)
+    # @Filmography = Movie.where ({ :director_id => @the_director})  
+
+
     
     render({ :template => "actor_templates/actor_details.html.erb"})
   end
